86da222adf018e372098ffea61f4687797681bec
StayStrong0100 <106422095+StayStrong0100@users.noreply.github.com>
2022-07-13 19:00:19 +0200

    Singleton mit eingebunden

--- src/diverses/HausListe.java
+++ src/diverses/HausListe.java
@@ -1,4 +1,16 @@
 package diverses;
 
 public class HausListe {
+    private HausListe(){
+    }
+    private static HausListe instance;
+    public static HausListe getInstance(){
+        if(instance == null) {
+            instance=new HausListe();
+        }
+        return instance;
+    }
+
+
+
 }
--- src/diverses/RaumListe.java
+++ src/diverses/RaumListe.java
@@ -1,4 +1,14 @@
 package diverses;
 
 public class RaumListe {
+    private RaumListe(){
+    }
+    private static RaumListe instance;
+    public static RaumListe getInstance(){
+        if(instance == null) {
+            instance=new RaumListe();
+        }
+        return instance;
+    }
+
 }
