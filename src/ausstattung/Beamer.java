34bab7471e9822ae85f041da65da3abfc18bd0a1
benkostka <mail@ben-kostka.de>
2022-07-20 01:16:54 +0200

    Exemplartyp von Beamer implementiert. Ein Verzicht auf die Klasse “Beamertyp” würde zu einer redundanten Speicherung von Attributwerten für die Objekte von Beamer führen.

--- src/ausstattung/Beamer.java
+++ src/ausstattung/Beamer.java
@@ -1,4 +1,12 @@
 package ausstattung;
 
+
 public class Beamer extends Ausstattungsmerkmal{
+
+    private final BeamerTyp exemplar;
+
+    public Beamer(BeamerTyp exemplar){
+        this.exemplar = new BeamerTyp(exemplar.getMarke(), exemplar.getModell(), exemplar.getLumen(), exemplar.isLautsprecher());
+    }
+
 }
--- src/ausstattung/BeamerTyp.java
+++ src/ausstattung/BeamerTyp.java
@@ -1,4 +1,49 @@
 package ausstattung;
 
 public class BeamerTyp {
+    private String marke;
+    private String modell;
+    private int lumen;
+    private boolean lautsprecher;
+
+    public BeamerTyp(String marke, String modell, int lumen, boolean lautsprecher){
+
+        this.marke = marke;
+        this.modell = modell;
+        this.lumen = lumen;
+        this.lautsprecher = lautsprecher;
+
+    }
+
+    public String getMarke() {
+        return marke;
+    }
+
+    public void setMarke(String marke) {
+        this.marke = marke;
+    }
+
+    public String getModell() {
+        return modell;
+    }
+
+    public void setModell(String modell) {
+        this.modell = modell;
+    }
+
+    public int getLumen() {
+        return lumen;
+    }
+
+    public void setLumen(int lumen) {
+        this.lumen = lumen;
+    }
+
+    public boolean isLautsprecher() {
+        return lautsprecher;
+    }
+
+    public void setLautsprecher(boolean lautsprecher) {
+        this.lautsprecher = lautsprecher;
+    }
 }
