be48a662246d0913e91d95cb3e7108b45d0b3445
benkostka <mail@ben-kostka.de>
2022-07-20 17:36:54 +0200

    Implementierung von Tisch und TischTyptyp.

--- src/ausstattung/Tisch.java
+++ src/ausstattung/Tisch.java
@@ -1,4 +1,18 @@
 package ausstattung;
 
 public class Tisch extends Ausstattungsmerkmal{
+
+    private TischTyp exemplarTyp;
+
+    public Tisch(TischTyp exemplarTyp){
+        this.exemplarTyp = exemplarTyp;
+    }
+
+    public TischTyp getExemplarTyp() {
+        return exemplarTyp;
+    }
+
+    public void setExemplarTyp(TischTyp exemplarTyp) {
+        this.exemplarTyp = exemplarTyp;
+    }
 }
--- src/ausstattung/TischTyp.java
+++ src/ausstattung/TischTyp.java
@@ -1,4 +1,58 @@
 package ausstattung;
 
 public class TischTyp {
+
+    private int anzahlPlaetze;
+    private boolean computertisch;
+    private boolean hoehenverstellbar;
+    private double breite;
+    private double laenge;
+
+    public TischTyp(int anzahlPlaetze, boolean computertisch, boolean hoehenverstellbar, double breite, double laenge){
+        this.anzahlPlaetze = anzahlPlaetze;
+        this.computertisch = computertisch;
+        this.hoehenverstellbar = hoehenverstellbar;
+        this.breite = breite;
+        this.laenge = laenge;
+    }
+
+    public int getAnzahlPlaetze() {
+        return anzahlPlaetze;
+    }
+
+    public void setAnzahlPlaetze(int anzahlPlaetze) {
+        this.anzahlPlaetze = anzahlPlaetze;
+    }
+
+    public boolean isComputertisch() {
+        return computertisch;
+    }
+
+    public void setComputertisch(boolean computertisch) {
+        this.computertisch = computertisch;
+    }
+
+    public boolean isHoehenverstellbar() {
+        return hoehenverstellbar;
+    }
+
+    public void setHoehenverstellbar(boolean hoehenverstellbar) {
+        this.hoehenverstellbar = hoehenverstellbar;
+    }
+
+    public double getBreite() {
+        return breite;
+    }
+
+    public void setBreite(double breite) {
+        this.breite = breite;
+    }
+
+    public double getLaenge() {
+        return laenge;
+    }
+
+    public void setLaenge(double laenge) {
+        this.laenge = laenge;
+    }
 }
