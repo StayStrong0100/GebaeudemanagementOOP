e33cd75f5ba0366d7f431baeff5801459aa64f3c
benkostka <mail@ben-kostka.de>
2022-07-20 17:23:29 +0200

    Implementierung von Smartboard und Smartboardtyp.

--- src/ausstattung/Smartboard.java
+++ src/ausstattung/Smartboard.java
@@ -1,4 +1,18 @@
 package ausstattung;
 
 public class Smartboard extends Tafel{
+
+    private SmartboardTyp exemplarTyp;
+
+    public Smartboard(SmartboardTyp exemplarTyp){
+        this.exemplarTyp = exemplarTyp;
+    }
+
+    public SmartboardTyp getExemplarTyp() {
+        return exemplarTyp;
+    }
+
+    public void setExemplarTyp(SmartboardTyp exemplarTyp) {
+        this.exemplarTyp = exemplarTyp;
+    }
 }
--- src/ausstattung/SmartboardTyp.java
+++ src/ausstattung/SmartboardTyp.java
@@ -1,4 +1,38 @@
 package ausstattung;
 
 public class SmartboardTyp {
+
+    private String marke;
+    private String modell;
+    private int lumen;
+
+    public SmartboardTyp(String marke, String modell, int lumen){
+        this.marke = marke;
+        this.modell = modell;
+        this.lumen = lumen;
+    }
+
+    public String getMarke() {
+        return marke;
+    }
+
+    public void setMarke(String marke) {
+        this.marke = marke;
+    }
+
+    public String getModell() {
+        return modell;
+    }
+
+    public void setModell(String modell) {
+        this.modell = modell;
+    }
+
+    public int getLumen() {
+        return lumen;
+    }
+
+    public void setLumen(int lumen) {
+        this.lumen = lumen;
+    }
 }
