6c1353ad4aad6e7f776c4c1a2312e1664475718d
benkostka <mail@ben-kostka.de>
2022-07-20 15:17:06 +0200

    Implementierung von Lautsprecher und LautsprecherTyp. Name für Variable Exemplar zu ExemplarTyp umbenennen.

--- src/ausstattung/Mikrofon.java
+++ src/ausstattung/Mikrofon.java
@@ -1,4 +1,18 @@
 package ausstattung;
 
 public class Mikrofon extends Ausstattungsmerkmal{
+
+    private MikrofonTyp exemplarTyp;
+
+    public Mikrofon(MikrofonTyp exemplarTyp){
+        this.exemplarTyp = exemplarTyp;
+    }
+
+    public MikrofonTyp getExemplarTyp() {
+        return exemplarTyp;
+    }
+
+    public void setExemplarTyp(MikrofonTyp exemplarTyp) {
+        this.exemplarTyp = exemplarTyp;
+    }
 }
--- src/ausstattung/MikrofonTyp.java
+++ src/ausstattung/MikrofonTyp.java
@@ -1,4 +1,48 @@
 package ausstattung;
 
 public class MikrofonTyp {
+
+    private String marke;
+    private String modell;
+    private boolean konferenzMikro;
+    private int empfindlichkeit;
+
+    public MikrofonTyp(String marke, String modell, boolean konferenzMikro, int empfindlichkeit){
+        this.marke = marke;
+        this.modell = modell;
+        this.konferenzMikro = konferenzMikro;
+        this.empfindlichkeit = empfindlichkeit;
+    }
+
+    public String getMarke() {
+        return marke;
+    }
+
+    public void setMarke(String marke) {
+        this.marke = marke;
+    }
+
+    public String getModell() {
+        return modell;
+    }
+
+    public void setModell(String modell) {
+        this.modell = modell;
+    }
+
+    public boolean isKonferenzMikro() {
+        return konferenzMikro;
+    }
+
+    public void setKonferenzMikro(boolean konferenzMikro) {
+        this.konferenzMikro = konferenzMikro;
+    }
+
+    public int getEmpfindlichkeit() {
+        return empfindlichkeit;
+    }
+
+    public void setEmpfindlichkeit(int empfindlichkeit) {
+        this.empfindlichkeit = empfindlichkeit;
+    }
 }
