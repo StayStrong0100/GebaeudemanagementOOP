0f3db60f6ff03a59d055d0c2e94b566a9fc17744
benkostka <mail@ben-kostka.de>
2022-07-20 17:27:13 +0200

    Implementierung von Stuhl und Stuhltyp.

--- src/ausstattung/Stuhl.java
+++ src/ausstattung/Stuhl.java
@@ -1,4 +1,18 @@
 package ausstattung;
 
 public class Stuhl extends Ausstattungsmerkmal{
+
+    private StuhlTyp exemplarTyp;
+
+    public Stuhl(StuhlTyp exemplarTyp){
+        this.exemplarTyp = exemplarTyp;
+    }
+
+    public StuhlTyp getExemplarTyp() {
+        return exemplarTyp;
+    }
+
+    public void setExemplarTyp(StuhlTyp exemplarTyp) {
+        this.exemplarTyp = exemplarTyp;
+    }
 }
--- src/ausstattung/StuhlTyp.java
+++ src/ausstattung/StuhlTyp.java
@@ -1,4 +1,28 @@
 package ausstattung;
 
 public class StuhlTyp {
+
+    private boolean drehstuhl;
+    private String modell;
+
+    public StuhlTyp(boolean drehstuhl, String modell){
+        this.drehstuhl = drehstuhl;
+        this.modell = modell;
+    }
+
+    public boolean isDrehstuhl() {
+        return drehstuhl;
+    }
+
+    public void setDrehstuhl(boolean drehstuhl) {
+        this.drehstuhl = drehstuhl;
+    }
+
+    public String getModell() {
+        return modell;
+    }
+
+    public void setModell(String modell) {
+        this.modell = modell;
+    }
 }
