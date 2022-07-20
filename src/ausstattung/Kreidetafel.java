575b411af065e739bafabac278fdd6b9fea3cbb1
benkostka <mail@ben-kostka.de>
2022-07-20 14:44:43 +0200

    Implementierung von Kreidetafel und KreidetafelTyp

--- src/ausstattung/Kreidetafel.java
+++ src/ausstattung/Kreidetafel.java
@@ -1,4 +1,18 @@
 package ausstattung;
 
 public class Kreidetafel extends Ausstattungsmerkmal{
+
+    private KreidetafelTyp exemplar;
+
+    public Kreidetafel(KreidetafelTyp exemplar){
+        this.exemplar = exemplar;
+    }
+
+    public KreidetafelTyp getExemplar() {
+        return exemplar;
+    }
+
+    public void setExemplar(KreidetafelTyp exemplar) {
+        this.exemplar = exemplar;
+    }
 }
--- src/ausstattung/KreidetafelTyp.java
+++ src/ausstattung/KreidetafelTyp.java
@@ -1,4 +1,48 @@
 package ausstattung;
 
 public class KreidetafelTyp {
+    private boolean wandtafel;
+    private boolean freitafel;
+    private double breite;
+    private double hoehe;
+
+    public KreidetafelTyp(boolean wandtafel, boolean freitafel, double breite, double hoehe){
+        this.wandtafel = wandtafel;
+        this.freitafel = freitafel;
+        this.breite = breite;
+        this.hoehe = hoehe;
+    }
+
+    public boolean isWandtafel() {
+        return wandtafel;
+    }
+
+    public void setWandtafel(boolean wandtafel) {
+        this.wandtafel = wandtafel;
+    }
+
+    public boolean isFreitafel() {
+        return freitafel;
+    }
+
+    public void setFreitafel(boolean freitafel) {
+        this.freitafel = freitafel;
+    }
+
+    public double getBreite() {
+        return breite;
+    }
+
+    public void setBreite(double breite) {
+        this.breite = breite;
+    }
+
+    public double getHoehe() {
+        return hoehe;
+    }
+
+    public void setHoehe(double hoehe) {
+        this.hoehe = hoehe;
+    }
 }
+
