1c587edb2ad817586a282cb3773ec350ea03a8a4
benkostka <mail@ben-kostka.de>
2022-07-23 20:22:55 +0200

    Anschaffungsdatum hinzugefügt.

--- src/ausstattung/Tafel.java
+++ src/ausstattung/Tafel.java
@@ -1,4 +1,9 @@
 package ausstattung;
 
+import java.util.Calendar;
+
 public abstract class Tafel extends Ausstattungsmerkmal{
+    public Tafel(Calendar anschaffungsdatum) {
+        super(anschaffungsdatum);
+    }
 }
