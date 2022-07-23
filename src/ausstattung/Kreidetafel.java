72468fb95f6bd91343109b0966aae4b0c928705d
benkostka <mail@ben-kostka.de>
2022-07-23 19:38:22 +0200

    Großschreibung in der toString() geändert

--- src/ausstattung/Kreidetafel.java
+++ src/ausstattung/Kreidetafel.java
@@ -1,10 +1,13 @@
 package ausstattung;
 
+import java.util.Calendar;
+
 public class Kreidetafel extends Tafel{
 
     private KreidetafelTyp exemplarTyp;
 
-    public Kreidetafel(KreidetafelTyp exemplarTyp){
+    public Kreidetafel(KreidetafelTyp exemplarTyp, Calendar anschaffungsdatum){
+        super(anschaffungsdatum);
         this.exemplarTyp = exemplarTyp;
     }
 
@@ -15,4 +18,6 @@ public class Kreidetafel extends Tafel{
     public void setExemplarTyp(KreidetafelTyp exemplarTyp) {
         this.exemplarTyp = exemplarTyp;
     }
+
+
 }
